b9308ad11f34d4eee458cf90efaf5958001c7ad5
Regis1272 <21206554+Regis1272@users.noreply.github.com>
2023-07-04 13:34:30 -0700

    Finished draw modes + buildGrid logic

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function buildGrid(size) {
+function clearGrid() {
+	while (display.firstChild) {
+		display.firstChild.remove();
+	}
+}
+
+function buildGrid(size, opacity) {
 
 
 	function makePixel() {
@@ -10,10 +16,17 @@ function buildGrid(size) {
 		pixel.style.display = 'flex';
 		pixel.style.width = '100%';
 		pixel.style.height = '100%';
+		if (opacity === true) {
+			pixel.style.backgroundColor = 'black';
+			pixel.style.opacity = '0';
+		}
 		// pixel.style.backgroundColor = bgColor;
 		// pixel.style.border = borderStyling;
 
 		pixel.addEventListener('mouseenter', () => {
+			drawOn(pixel, currentMode);
+		});
+		pixel.addEventListener('mousedown', () => {
 			drawOn(pixel, currentMode);
 		});
 
@@ -37,9 +50,7 @@ function buildGrid(size) {
 	// vvv Code      vvv
 	
 	// wipe current grid clean
-	while (display.firstChild) {
-		display.firstChild.remove();
-	}
+	clearGrid();
 	
 	for (let y = 1; y <= size; y++) { // new row
 		
@@ -58,28 +69,102 @@ function buildGrid(size) {
 
 }
 
+/* ---------- Drawing Functionality ---------- */
+
+
 function drawOn(selector, drawMode) {
-	selector.style.backgroundColor = drawMode;
+	// If left mouse button is down
+	if (event.buttons === 1) {
+		// draw in selected style
+		if (drawMode === drawMode_Greyscale) {
+			selector.style.opacity = drawMode(selector);
+		} else {
+			selector.style.backgroundColor = drawMode(selector);
+		}
+	}
 }
 
-let brushColor = 'purple';
 
-let drawMode_HardColor = function() {
+let drawMode_Palette = function(selector) {
+	console.log(selector.style.opacity);
 	return brushColor;
 }
 
-let drawMode_Greyscale = function() {
-	
+let drawMode_Greyscale = function(selector) {
+	console.log(selector.style.opacity);
+	if (selector.style.opacity < 1) {
+		let scale = Number(selector.style.opacity) + 0.1;
+		console.log(scale);
+		return scale;
+	}
 }
 
-const display = document.querySelector('.screen');
+let drawMode_Rainbow = function(selector) {
+	let r = Math.floor(Math.random()*255);
+	let g = Math.floor(Math.random()*255);
+	let b = Math.floor(Math.random()*255);
+	return `rgb(${r}, ${g}, ${b})`;
+}
 
+let brushColor = 'black';
+let currentMode = drawMode_Palette;
 const bgColor = 'white';
 const borderStyling = '0px solid black';
 
-let currentMode = drawMode_HardColor();
+/* ---------- Buttons + Event Listeners ---------- */
 
-buildGrid(8);
+// Set up display and user-interaction elements
+const display = document.querySelector('.screen');
+
+const clearBtn = document.getElementById('clear');
+const screenSlider = document.getElementById('screenSlider');
+
+const colorPkr = document.getElementById('colorPicker')
+const modePaletteBtn = document.getElementById('paletteBtn');
+const modeGreyscaleBtn = document.getElementById('greyscaleBtn');
+const modeRainbowBtn = document.getElementById('rainbowBtn');
+
+let palette = document.getElementById('palette');
+
+clearBtn.addEventListener('click', () => {
+	buildGrid(screenSlider.value);
+});
+
+screenSlider.addEventListener('change', () => {
+	buildGrid(screenSlider.value);
+});
+
+colorPkr.addEventListener('input', () => {
+	brushColor = colorPkr.value;
+});
+
+modePaletteBtn.addEventListener('click', () => {
+	if (currentMode !== drawMode_Palette) {
+		buildGrid(screenSlider.value, false);
+		palette.style.visibility = 'visible';
+		currentMode = drawMode_Palette;
+	}
+});
+
+modeGreyscaleBtn.addEventListener('click', () => {
+	if (currentMode !== drawMode_Greyscale) {
+		buildGrid(screenSlider.value, true);
+		palette.style.visibility = 'hidden';
+		currentMode = drawMode_Greyscale;
+	}
+});
+
+modeRainbowBtn.addEventListener('click', () => {
+	if (currentMode !== drawMode_Rainbow) {
+		buildGrid(screenSlider.value, false);
+		palette.style.visibility = 'hidden';
+		currentMode = drawMode_Rainbow
+	}
+});
+
+
+
+buildGrid(screenSlider.value);
 
 // build the grid
 // 	a single square will handle a hover event
